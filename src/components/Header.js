1621f4ad066a3319d078de7885ab95a7c3c16fb7
Tommaso Castagnola <tommaso.castagnola@hotmail.com>
2021-09-17 01:19:36 +0200

    Importante correzione header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,23 @@
 // import { useState, useEffect } from 'react'
 import React from 'react'
-// import { useAuth } from '../contexts/AuthContext'
+import { useAuth } from '../contexts/AuthContext'
 import { Link } from 'react-router-dom'
-
 
 const Header = () => {
 
-    // const { currentUser, logout } = useAuth()
+    const { currentUser } = useAuth()
 
     return (
         <header className='header'>
             <h1>Charter</h1>
-            <div className='session-head'>
-                {/* {error && <Alert variant='danger'> {error} </Alert>} */}
-                {/* <strong>Email: {currentUser.email}</strong> */}
-                <Link to='/update-profile' style={{ color: 'green', textDecoration: 'none' }}>Update Credentials</Link>
-            </div>
+            {currentUser ?
+                <div className='session-head'>
+                    {/* {error && <Alert variant='danger'> {error} </Alert>} */}
+                    {/* <strong>Email: {currentUser.email}</strong> */}
+                    <Link to='/update-profile' style={{ color: 'green', textDecoration: 'none' }}>Update Credentials</Link>
+                </div> :
+                <></>
+            }
         </header>
     )
 }
