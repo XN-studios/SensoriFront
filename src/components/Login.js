40af24d9bb04454167950ae74c05e8362ef1c70e
Tommaso Castagnola <tommaso.castagnola@hotmail.com>
2021-09-18 00:02:02 +0200

    Vietato l'accesso al Login se già autenticati, con reindirizzamento alla dashboard

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,12 @@
 import React, { useRef, useState } from 'react'
 import { Form, Button, Card, Alert } from 'react-bootstrap'
 import { useAuth } from "../contexts/AuthContext"
-import { Link, useHistory } from 'react-router-dom'
+import { Link, useHistory, Redirect } from 'react-router-dom'
 
 export default function Login() {
     const emailRef = useRef()
     const passwordRef = useRef()
-    const { login } = useAuth()
+    const { currentUser, login } = useAuth()
     const [error, setError] = useState('')
     const [loading, setLoading] = useState(false)
     const history = useHistory()
@@ -30,32 +30,36 @@ export default function Login() {
     }
 
     return (
-        <div>
-            <Card className="log-in">
-                <Card.Body>
-                    <h2>Log In</h2>
-                    <Form onSubmit={handleSubmit}>
-                        {error && <Alert variant="danger">{error}</Alert>}
-                        <Form.Group id="email">
-                            <Form.Label>Email</Form.Label>
-                            <Form.Control type="email" ref={emailRef} required />
-                        </Form.Group>
-                        <Form.Group id="password">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" ref={passwordRef} required />
-                        </Form.Group>
-                        <br></br>
-                        <Button disabled={loading} className="w-100" type="submit">Log In</Button>
-                    </Form>
-                    <div className="text-center mt-2">
-                        <Link to="/forgot-password">Password dimenticata?</Link>
-                    </div>
-                </Card.Body>
-            </Card>
-            <div className="text-center mt-2">
-                Non hai ancora un account?<Link to='/signup'>Creane uno</Link>
-            </div>
-        </div>
+        <>
+            {currentUser ?
+            <Redirect to="/" /> :
+            <div>
+                <Card className="log-in">
+                    <Card.Body>
+                        <h2>Log In</h2>
+                        <Form onSubmit={handleSubmit}>
+                            {error && <Alert variant="danger">{error}</Alert>}
+                            <Form.Group id="email">
+                                <Form.Label>Email</Form.Label>
+                                <Form.Control type="email" ref={emailRef} required />
+                            </Form.Group>
+                            <Form.Group id="password">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control type="password" ref={passwordRef} required />
+                            </Form.Group>
+                            <br></br>
+                            <Button disabled={loading} className="w-100" type="submit">Log In</Button>
+                        </Form>
+                        <div className="text-center mt-2">
+                            <Link to="/forgot-password">Password dimenticata?</Link>
+                        </div>
+                    </Card.Body>
+                </Card>
+                <div className="text-center mt-2">
+                    Non hai ancora un account?<Link to='/signup'>Creane uno</Link>
+                </div>
+            </div>}
+        </>
     )
 }
 
